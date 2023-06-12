324bda5f8af2f33f30f72c21d24dc97b51c0c613
Francisco Javier Suarez Verdugo <46821988+Ulzahk@users.noreply.github.com>
2023-06-12 17:00:11 -0500

    feat(miscellaneous): add misc functionalities (#6)

---
+++ app/misc/tls-client.js
@@ -0,0 +1,32 @@
+/*
+ * Example TLS client
+ * Connects to port 6000 and sends the word "ping" to servers
+ *
+ */
+
+// Dependencies
+const tls = require('tls');
+const fs = require('fs');
+const path = require('path');
+
+// Define the message to send
+const outboundMessage = 'ping';
+
+
+// Client options
+const options = {
+  'ca': [fs.readFileSync(path.join(__dirname, '/../https/cert.pem'))] // Only required because we're using a self-signed cert)
+};
+
+// Create the client
+const client = tls.connect(6000, options, function () {
+  // Send the message
+  client.write(outboundMessage);
+});
+
+// When the server writes back, log what it says then kill the client
+client.on('data', function (inboundMessage) {
+  const messageString = inboundMessage.toString();
+  console.log("I wrote " + outboundMessage + " and they said " + messageString);
+  client.end();
+});
